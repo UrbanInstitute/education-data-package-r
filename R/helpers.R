a4e8f466d87dc9a75e645253efcd0821d76e9913
khueyama <kueyama@urban.org>
2018-04-08 17:05:03 -0400

    first cut attempt at parallelisation over api calls

--- R/helpers.R
+++ R/helpers.R
@@ -1,4 +1,4 @@
-get_data <- function(url) {
+get_data_old <- function(url) {
   request <- httr::GET(url)
   resp <- jsonlite::fromJSON(rawToChar(request$content))
   pages <- ceiling(resp$count / 100)
@@ -18,4 +18,49 @@ get_data <- function(url) {
 
   return(df)
 
+}
+
+get_page_data <- function(page_url) {
+  print(page_url)
+  request <- httr::GET(page_url)
+  resp <- jsonlite::fromJSON(rawToChar(request$content))
+  df <- resp$results
+  return(df)
+}
+
+get_data <- function(url) {
+  request <- httr::GET(url)
+  resp <- jsonlite::fromJSON(rawToChar(request$content))
+  pages <- ceiling(resp$count / 100)
+
+  if (pages > 2) {
+    page_urls <- paste0(url, '?page=', 2:pages)
+    page_urls <- c(url, page_urls)
+  } else {
+    return(resp$results)
+  }
+
+  dfs <- lapply(page_urls, get_page_data)
+  df <- do.call(rbind, dfs)
+  return(df)
+}
+
+get_data_parallel <- function(url) {
+  request <- httr::GET(url)
+  resp <- jsonlite::fromJSON(rawToChar(request$content))
+  pages <- ceiling(resp$count / 100)
+
+  if (pages > 2) {
+    page_urls <- paste0(url, '?page=', 2:pages)
+    page_urls <- c(url, page_urls)
+  } else {
+    return(resp$results)
+  }
+
+  cl <- parallel::makeCluster(6)
+  dfs <- parallel::parLapply(cl, page_urls, get_page_data)
+  parallel::stopCluster(cl)
+
+  df<- do.call(rbind, dfs)
+  return(df)
 }

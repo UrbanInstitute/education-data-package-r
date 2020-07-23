163cc8bac45755a820ec3066c59a3f15c64ea771
khueyama <khueyama@gmail.com>
2020-07-23 17:29:20 -0400

    add unit tests for multiple filter values

--- tests/testthat/test-validation.R
+++ tests/testthat/test-validation.R
@@ -14,14 +14,14 @@ test_that('invalid api arguments return an error message', {
                                   source = 'ccd',
                                   topic = 'enrollment',
                                   by = list('fake')))
-  # expect_error(get_education_data(level = 'schools',
-  #                                 source = 'ccd',
-  #                                 topic = 'enrollment',
-  #                                 filters = list(fake = 'value')))
-  # expect_error(get_education_data(level = 'schools',
-  #                                 source = 'ccd',
-  #                                 topic = 'enrollment',
-  #                                 filters = list(year = 1899)))
+  expect_error(get_education_data(level = 'schools',
+                                  source = 'ccd',
+                                  topic = 'enrollment',
+                                  filters = list(fake = 'value')))
+  expect_error(get_education_data(level = 'schools',
+                                  source = 'ccd',
+                                  topic = 'enrollment',
+                                  filters = list(year = 1899)))
   expect_warning(get_education_data(level = 'schools',
                                     source = 'ccd',
                                     topic = 'directory',
